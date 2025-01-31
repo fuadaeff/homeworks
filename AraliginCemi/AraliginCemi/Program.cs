c7fc84fb9db0174b794b6a3f88842deb4965f0ca
fuadaeff <fabdulla057@gmail.com>
2025-01-31 19:37:51 +0400

    Add files via upload

---
+++ AraliginCemi/AraliginCemi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection.Emit;
+
+namespace AraliginCemi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            int sum = 0;
+            Label:
+            Console.WriteLine("Cemin tapmaq istediyiniz araligi daxil edin:");
+            int i = int.Parse(Console.ReadLine());
+            int j = int.Parse(Console.ReadLine());
+            if (j > numbers.Length + 1)
+            {
+                Console.WriteLine("Siz boyuk indeks daxil etdiniz!Yeniden cehd edin!");
+                goto Label;
+            }
+            for (int k = i - 1; k < j - 1; k++) {
+                sum = sum + numbers[k];
+            }
+            Console.WriteLine($"Daxil etdiyiniz araligin cemi: {sum}");
+        }
+    }
+}
