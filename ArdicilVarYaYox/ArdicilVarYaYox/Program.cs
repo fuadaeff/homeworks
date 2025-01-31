8d242960760a9bfadfa943b949534450f2f6e53c
fuadaeff <fabdulla057@gmail.com>
2025-01-31 19:33:53 +0400

    Add files via upload

---
+++ ArdicilVarYaYox/ArdicilVarYaYox/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ArdicilVarYaYox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 3, 5, 6, 7, 8, 9 };
+            int count = 0;
+            for (int i = 1; i < numbers.Length; i++) {
+                if (numbers[i - 1] == numbers[i]) {
+                    Console.WriteLine("Ardicil eyni eded var!");
+                    count++;
+                }
+            }
+            if (count == 0) {
+                Console.WriteLine("Ardicil eyni eded yoxdur!");
+            }
+        }
+    }
+}
