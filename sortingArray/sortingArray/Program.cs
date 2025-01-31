3ee37500f9d3a93d9f39837a08cce797b128b6c6
fuadaeff <fabdulla057@gmail.com>
2025-01-31 19:36:33 +0400

    Add files via upload

---
+++ sortingArray/sortingArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.Emit;
+
+namespace sortingArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 5, 6, 3, 8, 7 };
+            for (int i = 1; i < numbers.Length; i++) {
+                if (numbers[i - 1] > numbers[i]) {
+                    numbers[i - 1] = numbers[i];
+                }
+            }
+            foreach (int number in numbers) {
+                Console.WriteLine(number);
+            }
+        }   
+    }
+}
