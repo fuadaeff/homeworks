137ccee8e31e2a2360b207b11a0d37de6e94c5d7
fuadaeff <fabdulla057@gmail.com>
2025-01-31 19:32:53 +0400

    Add files via upload

---
+++ ArrayChecking/ArrayChecking/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ArrayChecking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checker = true;
+            int[] numbers1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] numbers2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < numbers1.Length; i++) {
+                for (int j = 0; j < numbers2.Length; j++) {
+                    if (numbers1[i] == numbers2[j])
+                    {
+                        checker = true;
+                    }
+                    else
+                    {
+                        checker = false;
+                    }
+                }
+            }
+            Console.WriteLine(checker);
+        }
+    }
+}
